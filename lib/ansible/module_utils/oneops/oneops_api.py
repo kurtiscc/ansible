451512a5e2103fe0571b17696c65d383e133f1f3
Samuel Hyman <Samuel.Hyman@walmart.com>
2019-12-12 12:59:04 -0600

    adding testcase for json in fetch_oneops_api test

--- lib/ansible/module_utils/oneops/oneops_api.py
+++ lib/ansible/module_utils/oneops/oneops_api.py
@@ -1,9 +1,9 @@
 from ansible.module_utils import urls
 
-def fetch_oneops_api(module_params, uri='/', method='GET', data=None, json=None):
-  url = 'https://%s/%s' %(module_params['oneops_host'], uri.lstrip('/'))
+def fetch_oneops_api(module, uri='/', method='GET', data=None, json=None):
+  url = 'https://%s/%s' %(module.params['oneops_host'], uri.lstrip('/'))
 
   if not data and json:
     data = module.jsonify(json)
 
-  return urls.open_url(url=url, method=method, data=data, force_basic_auth=True, url_username=module_params['api_key'], url_password='')
+  return urls.open_url(url=url, method=method, data=data, force_basic_auth=True, url_username=module.params['api_key'], url_password='')
--- test/units/module_utils/oneops/test_oneops_api.py
+++ test/units/module_utils/oneops/test_oneops_api.py
@@ -2,6 +2,8 @@ import json
 import pytest
 
 from ansible.module_utils.oneops import oneops_api
+from ansible.module_utils.basic import AnsibleModule
+from units.modules.utils import set_module_args
 
 mock_module_params = {
     'oneops_host': 'foo.bar.com',
@@ -9,7 +11,27 @@ mock_module_params = {
 }
 
 def test_fetch_oneops_api(mocker):
+    set_module_args({})
+
+    module = AnsibleModule(argument_spec=dict(
+        oneops_host=dict(type='str', default='foo.bar.com'),
+        api_key=dict(type='str', default='foobar')
+    ))
+
     open_url_mock = mocker.patch('ansible.module_utils.urls.open_url')
-    oneops_api.fetch_oneops_api(mock_module_params, uri='/')
+    oneops_api.fetch_oneops_api(module, uri='/')
     open_url_mock.assert_called_once_with(url='https://foo.bar.com/', data=None, method='GET',
+                                     url_username='foobar', url_password='', force_basic_auth=True)
+
+def test_fetch_oneops_api_with_json(mocker):
+    set_module_args({})
+
+    module = AnsibleModule(argument_spec=dict(
+        oneops_host=dict(type='str', default='foo.bar.com'),
+        api_key=dict(type='str', default='foobar')
+    ))
+
+    open_url_mock = mocker.patch('ansible.module_utils.urls.open_url')
+    oneops_api.fetch_oneops_api(module, uri='/', json={"message":"test"})
+    open_url_mock.assert_called_once_with(url='https://foo.bar.com/', data='{"message": "test"}', method='GET',
                                      url_username='foobar', url_password='', force_basic_auth=True)
